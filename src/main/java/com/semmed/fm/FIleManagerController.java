d69bfce13a8bbdd65e75aa758479654bff41382a
Semmed Kosayev <semmedkosayevbusiness@gmail.com>
2025-01-22 16:28:47 +0400

    added file download

--- src/main/java/com/semmed/fm/FIleManagerController.java
+++ src/main/java/com/semmed/fm/FIleManagerController.java
@@ -1,11 +1,20 @@
 package com.semmed.fm;
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,5 +37,21 @@ public class FIleManagerController {
             logger.log(Level.SEVERE, "Exception during upload", e);
         }
         return false;
+    }
+
+    @GetMapping("/download")
+    public ResponseEntity<Resource> downloadFile(@RequestParam("fileName") String fileName) {
+        try {
+            File downloadFile = service.getDownloadFile(fileName);
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                    .contentLength(downloadFile.length())
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .body(new InputStreamResource(Files.newInputStream(downloadFile.toPath())));
+        } catch (FileNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/semmed/fm/FileStorageService.java
+++ src/main/java/com/semmed/fm/FileStorageService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
@@ -26,5 +27,23 @@ public class FileStorageService {
         }
 
         Files.copy(fileToSave.getInputStream(), targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    public File getDownloadFile(String fileName) throws FileNotFoundException {
+        if (fileName == null) {
+            throw new NullPointerException("fileName is not provided.");
+        }
+
+        var fileToDownload = new File(STORAGE_DIRECTORY + File.separator + fileName);
+
+        if (!Objects.equals(fileToDownload.getParent(), STORAGE_DIRECTORY)) {
+            throw new SecurityException("unsupported file name");
+        }
+
+        if (!fileToDownload.exists()) {
+            throw new FileNotFoundException("no file found for: " + fileName);
+        }
+
+        return fileToDownload;
     }
 }
